cc4935b282c21fedd1dbd63d015162d0b8f894ef
venarunxp <69140004+venarunxp@users.noreply.github.com>
2023-02-05 23:31:19 +0530

    Update vdrun.sh

--- vdrun.sh
+++ vdrun.sh
@@ -11,6 +11,6 @@ cd run
 brew install --cask android-commandlinetools
 export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
 avdmanager create avd -n test -k "system-images;android-30;google_atd;arm64-v8a"
-/usr/local/share/android-sdk/tools/emulator -avd test
+/usr/local/share/android-sdk/emulator/emulator -avd test
 adb devices
 exit 0
