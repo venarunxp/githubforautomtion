4af55f0e1b7385a024600405262c306dbdf64698
venarunxp <69140004+venarunxp@users.noreply.github.com>
2023-02-05 23:16:47 +0530

    Update vdrun.sh

--- vdrun.sh
+++ vdrun.sh
@@ -8,8 +8,11 @@
 # http://4pda.ru/forum/index.php?showtopic=318487
 mkdir run
 cd run
+brew install --cask android-commandlinetools
 brew install --cask android-sdk
-sdkmanager --install "system-images;android-30;aosp_atd;arm64-v8a"
-y
+export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
+avdmanager --verbose create avd --force --name "pixel_5_api30_google_atd_emulator" --package "system-images;android-30;google_atd;arm64-v8a" --tag "google_atd" --abi "arm64-v8a" --device "pixel_5"avdmanager --verbose create avd --force --name "generic_api30_aosp_atd_emulator" --package "system-images;android-30;aosp_atd;x86" --tag "aosp_atd" --abi "x86"
+avdmanager create avd -n test -k "system-images;android-30;google_atd;arm64-v8a"
+/usr/local/share/android-sdk/tools/emulator -avd test
 adb devices
 exit 0
